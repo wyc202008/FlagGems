2845d622242c19cfacecb3424c45c59242e9b8ec
strongspoon <strongspoon@outlook.com>
2025-03-25 13:30:01 +0800

    [format]

--- src/flag_gems/fused/__init__.py
+++ src/flag_gems/fused/__init__.py
@@ -1,9 +1,9 @@
+from .flash_mla import flash_mla
 from .gelu_and_mul import gelu_and_mul
 from .rotary_embedding import apply_rotary_pos_emb
 from .silu_and_mul import silu_and_mul
 from .skip_layernorm import skip_layer_norm
 from .skip_rms_norm import skip_rms_norm
-from .flash_mla import flash_mla
 
 __all__ = [
     "apply_rotary_pos_emb",
