e311b840cb07434ded90e2681143e7a7fb8a38f6
strongspoon <strongspoon@outlook.com>
2025-03-25 02:52:22 +0000

    [operator] optimzie flash_mla_triton

--- src/flag_gems/fused/__init__.py
+++ src/flag_gems/fused/__init__.py
@@ -3,6 +3,7 @@ from .rotary_embedding import apply_rotary_pos_emb
 from .silu_and_mul import silu_and_mul
 from .skip_layernorm import skip_layer_norm
 from .skip_rms_norm import skip_rms_norm
+from .flash_mla import flash_mla
 
 __all__ = [
     "apply_rotary_pos_emb",
@@ -10,4 +11,5 @@ __all__ = [
     "skip_rms_norm",
     "silu_and_mul",
     "gelu_and_mul",
+    "flash_mla",
 ]
